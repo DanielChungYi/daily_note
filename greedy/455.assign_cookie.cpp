e23856cd16b4b56d1441e4cbbf555aebf64b3150
Daniel CY <ms0227162@gmail.com>
2023-12-19 02:29:35 +0900

    [greedy]: adter nigata

---
+++ greedy/455.assign_cookie.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int result = 0;
+        
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int index = s.size() - 1;
+        for (int i = g.size() - 1; i >= 0; i--) {
+            if(index >= 0 && s[index] >= g[i]) {
+                result++;
+                index--; 
+            }
+        }
+        return result;
+    }
+};
+
