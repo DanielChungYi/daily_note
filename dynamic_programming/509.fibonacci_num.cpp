4b1e5148f44432124a3cbc1eb7fc39810c9bdf3b
Daniel CY <ms0227162@gmail.com>
2024-01-10 21:58:46 +0900

    [dp]: dp init

---
+++ dynamic_programming/509.fibonacci_num.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fib(int n) {
+        int *dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        if(n <= 1) return dp[n];
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        int result = dp[n];
+        delete[] dp;
+        return result;
+    }
+};
+
